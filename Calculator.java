fbc34b5edd7bbf83b45e9dee73a2008e5ee67687
IrinaBogatko95 <irinabogatko95@gmail.com>
2016-10-03 09:01:54 +0300

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -35,27 +35,27 @@ public class Calculator {
             }
         }
 }
-     /**
-     *  Sum of two numbers
-     */
+        /**
+         *  Sum of two numbers
+         */
         public static double calculateSum(double a, double b){
             return a + b;
         }
-     /**
-     * Difference of two numbers
-     */
+        /**
+         * Difference of two numbers
+         */
         public static double calculateDifference(double a, double b){
             return a - b;
         }
-     /**
-     * Composition of two numbers
-     */
+        /**
+         * Composition of two numbers
+         */
         public static double calculateComposition(double a, double b){
             return a * b;
         }
-     /**
-     * Quotient of two numbers
-     */
+        /**
+         * Quotient of two numbers
+         */
         public static double calculateQuotient(double a, double b){
             return a / b;
         }
