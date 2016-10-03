ce773b232a785f9c98df72dc4ea25ec96de4bdb6
IrinaBogatko95 <irinabogatko95@gmail.com>
2016-10-03 22:32:45 +0300

    Add files via upload

--- RootsOfEquation.java
+++ RootsOfEquation.java
@@ -3,27 +3,25 @@ import java.util.Scanner;
  * Class has methods which output roots of the equation ax2 + bx + c = 0; a, b, c input from the keyboard
  */
 public class RootsOfEquation {
+    public static final double MY_CONST = 10e-10;
         /**
          * Output roots of the quadratic equation ax2 + bx + c = 0
          */
         public static void main(String[] args){
             Scanner sc = new Scanner(System.in);
-
             System.out.println("Enter a:");
             double a = sc.nextDouble();
-
             System.out.println("Enter b:");
             double b = sc.nextDouble();
-
             System.out.println("Enter c:");
             double c = sc.nextDouble();
 
-            if(a < 0.00001 && a > -0.00001){
+            if(a < MY_CONST && a > -MY_CONST) {
                 System.out.println("The coefficient at the first term can not equal zero. Try again.");
             } else {
                 System.out.println("Your equation: (" + a + ") * x ^ 2" + " + (" + b + ") * x " + " + (" + c + ")");
-                roots(a, b, c);
             }
+            roots(a, b, c);
     }
     /**
      * Counting discriminant and finding the roots of the equation
@@ -34,7 +32,7 @@ public class RootsOfEquation {
             double d = Math.pow(b, 2) - 4  * a * c;
             if ( d < 0){
                 System.out.println("D < 0, the equation does not have roots");
-            } else if (d < 0.00001 && d > -0.00001){
+            } else if (d < MY_CONST && d > -MY_CONST){
                 mas[0] = (- b /(2 * a));
                 System.out.println("D = 0, the equation have one root");
                 System.out.println("x = " + mas[0]);
