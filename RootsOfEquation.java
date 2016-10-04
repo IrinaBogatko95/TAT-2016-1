4be47f9568f0dc8453bc8fd8ff54e6e7c07dcba5
IrinaBogatko95 <irinabogatko95@gmail.com>
2016-10-04 23:01:13 +0300

    Add files via upload

--- RootsOfEquation.java
+++ RootsOfEquation.java
@@ -1,47 +1,73 @@
+package Task_4;
 import java.util.Scanner;
 /**
  * Class has methods which output roots of the equation ax2 + bx + c = 0; a, b, c input from the keyboard
+ * @author Irina Bogatko
+ * @version 1.0
+ * @since 04-10-2016
  */
 public class RootsOfEquation {
-    public static final double MY_CONST = 10e-10;
-        /**
-         * Output roots of the quadratic equation ax2 + bx + c = 0
-         */
-        public static void main(String[] args){
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter a:");
-            double a = sc.nextDouble();
-            System.out.println("Enter b:");
-            double b = sc.nextDouble();
-            System.out.println("Enter c:");
-            double c = sc.nextDouble();
-
-            if(a < MY_CONST && a > -MY_CONST) {
-                System.out.println("The coefficient at the first term can not equal zero. Try again.");
-            } else {
-                System.out.println("Your equation: (" + a + ") * x ^ 2" + " + (" + b + ") * x " + " + (" + c + ")");
-            }
-            roots(a, b, c);
+    /**
+     * Output roots of the quadratic equation ax2 + bx + c = 0
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+        double[] coeff = new double[3];
+        entry(coeff);
+        System.out.println("Your equation: (" + coeff[0] + ") * x ^ 2" + " + (" + coeff[1] + ") * x " + " + (" + coeff[2] + ")");
+        roots(coeff);
     }
     /**
      * Counting discriminant and finding the roots of the equation
+     * @param coeff array with input values(a, b, c)
      */
-    public static void roots(double a, double b, double c){
-            double[] mas = new double[2];
-            //discriminant calculation
-            double d = Math.pow(b, 2) - 4  * a * c;
-            if ( d < 0){
-                System.out.println("D < 0, the equation does not have roots");
-            } else if (d < MY_CONST && d > -MY_CONST){
-                mas[0] = (- b /(2 * a));
-                System.out.println("D = 0, the equation have one root");
-                System.out.println("x = " + mas[0]);
-            } else if(d > 0) {
-                mas[0] = (-b + Math.sqrt(d)) / (2 * a);
-                mas[1] = (-b - Math.sqrt(d)) / (2 * a);
-                System.out.println("D > 0, the equation have two roots:");
-                System.out.println("x1 = " + mas[0]);
-                System.out.println("x2 = " + mas[1]);
+    public static void roots(double[] coeff) {
+        //discriminant calculation
+        double x1;
+        double x2;
+        double d = Math.pow(coeff[1], 2) - 4  * coeff[0] * coeff[2];
+        if ( d < 0) {
+            System.out.println("D < 0, the equation does not have roots");
+        } else if (zero(d)) {
+            x1 = (- coeff[1] /(2 * coeff[0]));
+            System.out.println("D = 0, the equation have one root");
+            System.out.println("x = " + x1);
+        } else if( d > 0 ) {
+            x1 = (-coeff[1] + Math.sqrt(d)) / (2 * coeff[0]);
+            x2 = (-coeff[1] - Math.sqrt(d)) / (2 * coeff[0]);
+            System.out.println("D > 0, the equation have two roots:");
+            System.out.println("x1 = " + x1);
+            System.out.println("x2 = " + x2);
+        }
+    }
+    /**
+     * Method return true if "a" equal to zero
+     * @param a number to check the vanishing
+     */
+    private static boolean zero (double a) {
+        if(Double.isNaN(1. / a)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    /**
+     * expects the entering of values from the user, returns this values
+     * @param coeff array of input values
+     */
+    public static double[] entry(double[] coeff) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            for(int i=0; i < coeff.length; i++) {
+                System.out.println("Enter " + (i + 1) + " coeff:");
+                coeff[i] = sc.nextDouble();
             }
+            if(coeff[0]==0) {
+                System.out.println("The coefficient at the first term can not equal zero. Try again.");
+            }
+        } catch(Exception ex) {
+            System.out.println("You entered not a number. Please, enter number");
+        }
+        return coeff;
     }
 }
--- Triangle.java
+++ Triangle.java
@@ -1,55 +1,62 @@
-
+package Task_4;
 import java.util.Scanner;
 /**
- * Output on the screen whether the  triangle equilateral, isosceles or the usual.
+ * Output on the screen whether the  triangle equilateral, isosceles or the usual
+ * @author Irina Bogatko
+ * @version 1.0
+ * @since 04-10-2016
  */
 public class Triangle {
     /**
-     * Waiting for input values from the keyboard, challenge "checks" method
+     * challenge "valuesEntering" and "kindOfTriangle" methods
+     * @param args command-line arguments
      */
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter the first side of the triangle:");
-        double a = sc.nextDouble();
-
-        System.out.println("Enter the second side of the triangle:");
-        double b = sc.nextDouble();
-
-        System.out.println("Enter the third side of the triangle:");
-        double c = sc.nextDouble();
-
-        checks(a, b, c);
+    public static void main(String[] args) {
+        double[] values = new double[3];
+        valuesEntering(values);
+        kindOfTriangle(values);
     }
     /**
-     *  Checks for the existence of a triangle
+     * Determining whether the triangle equilateral, isosceles or the usual.
+     * @param values array with input values
      */
-    public static void checks(double a, double b, double c){
-        if (a + b < c || a + c < b || b + c < a){
-            System.out.println("Triangle with such sides does not exist!");
-        } else if(a <= 0 || b <= 0 || c <= 0){
-            System.out.println("Side of the triangle cannot be equal to zero or less then zero!");
-        } else {
-            kindOfTriangle(a, b, c);
-        }
-    }
-    /**
-     * Determining whether the  triangle equilateral, isosceles or the usual.
-     */
-    public static void kindOfTriangle(double a, double b, double c){
+    public static void kindOfTriangle(double[] values) {
         //side comparison
-        int compare_ab = Double.compare(a, b);
-        int compare_ac = Double.compare(a, c);
-        int compare_cb = Double.compare(c, b);
+        int compare_ab = Double.compare(values[0], values[1]);
+        int compare_ac = Double.compare(values[0], values[2]);
+        int compare_cb = Double.compare(values[2], values[1]);
         if(compare_ab == 0 && compare_ac == 0){
             System.out.println("This triangle is equilateral");
         }
         else if((compare_ab == 0 && compare_ac != 0 ) || (compare_ac == 0 && compare_cb != 0)
                 || (compare_cb == 0 && compare_ab !=0)){
             System.out.println("This triangle is isosceles");
-        }
-        else{
+        } else {
             System.out.println("This triangle is ordinary");
         }
+    }
+    /**
+     * Expects the entering of values from the user, returns this values
+     * and verifies the existence of a triangle
+     * @param values array of input values
+     */
+    public static double[] valuesEntering(double[] values) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            for (int i = 0; i < values.length; i++) {
+                System.out.println("Enter " + (i + 1) + " side of the triangle:");
+                values[i] = sc.nextDouble();
+            }
+            //checks for equality sides to zero and the existence of a triangle
+            if (values[0] <= 0 || values[1] <= 0 || values[2] <= 0) {
+                System.out.println("Side of the triangle cannot be equal to zero or less then zero!");
+            } else if (values[0] + values[1] < values[2] || values[0] + values[2] < values[1] || values[1] + values[2] < values[0]) {
+                System.out.println("Triangle with such sides does not exist!");
+            }
+        } catch (Exception ex) {
+            System.out.println("You entered not a number. Please, enter number");
+            System.exit(1);
+        }
+        return values;
     }
 }
