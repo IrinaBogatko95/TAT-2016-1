211ead7af795d8202b50492ccbedfa42d7752f07
IrinaBogatko95 <irinabogatko95@gmail.com>
2016-11-10 16:11:15 +0300

    Add TestXmlFileReader

--- TriangleTestNG/test/java/TriangleTest.java
+++ TriangleTestNG/test/java/TriangleTest.java
@@ -1,41 +1,16 @@
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import org.w3c.dom.Document;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.NodeList;
 import triangle.Triangle;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.File;
 
 import static org.testng.Assert.assertEquals;
 
 public class TriangleTest {
-    public static final String PATH = ".\\PositiveDataTriangle.xml";
     public static final String POSITIVE = "positive";
-    public static final String EXPECTED = "expected";
-    public static final String SIDE_A = "side_a";
-    public static final String SIDE_B = "side_b";
-    public static final String SIDE_C = "side_c";
 
     @DataProvider(name = "Detect triangle provider")
     public Object[][] positiveKindTriangle() throws Exception {
-        File inputFile = new File(PATH);
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        Document document = builder.parse(inputFile);
-        NodeList nodeList = document.getElementsByTagName(POSITIVE);
-        Object[][] result = new Object[nodeList.getLength()][];
-        for (int i = 0; i < nodeList.getLength(); i++) {
-            NamedNodeMap map = nodeList.item(i).getAttributes();
-            result[i] = new Object[]{
-                    map.getNamedItem(EXPECTED).getNodeValue(),
-                    getDoubleSide(map.getNamedItem(SIDE_A).getNodeValue()),
-                    getDoubleSide(map.getNamedItem(SIDE_B).getNodeValue()),
-                    getDoubleSide(map.getNamedItem(SIDE_C).getNodeValue()),
-            };
-        }
+        TestXmlFileReader testXmlFileReader = new TestXmlFileReader();
+        Object[][] result = testXmlFileReader.readSidesXmlFileWithExpected(POSITIVE);
         return result;
     }
 
@@ -44,15 +19,5 @@ public class TriangleTest {
         Triangle triangle = new Triangle(a, b, c);
         triangle.kindOfTriangle(triangle);
         assertEquals(triangle.getType(), expected);
-    }
-
-    public double getDoubleSide(String side) {
-        if (side.equals("Double.MAX_VALUE")) {
-            return Double.MAX_VALUE;
-        }
-        if (side.equals("Double.MIN_VALUE")) {
-            return Double.MIN_VALUE;
-        }
-        return Double.parseDouble(side);
     }
 }
--- TriangleTestNG/test/java/ValidatorTest.java
+++ TriangleTestNG/test/java/ValidatorTest.java
@@ -1,39 +1,15 @@
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import org.w3c.dom.Document;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.NodeList;
 import triangle.Triangle;
 import triangle.Validator;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.File;
-
 public class ValidatorTest {
-    public static final String PATH = ".\\NegativeDataTriangle.xml";
     public static final String NEGATIVE = "negative";
-    public static final String SIDE_A = "side_a";
-    public static final String SIDE_B = "side_b";
-    public static final String SIDE_C = "side_c";
 
     @DataProvider(name = "Negative check triangle provider")
     public Object[][] negativeCheckExistenceTriangle() throws Exception {
-        File inputFile = new File(PATH);
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-
-        Document document = builder.parse(inputFile);
-        NodeList nodeList = document.getElementsByTagName(NEGATIVE);
-        Object[][] result = new Double[nodeList.getLength()][];
-        for (int i = 0; i < nodeList.getLength(); i++) {
-            NamedNodeMap map = nodeList.item(i).getAttributes();
-            result[i] = new Double[]{
-                    map.getNamedItem(SIDE_A) == null ? null : getDoubleSide(map.getNamedItem(SIDE_A).getNodeValue()),
-                    map.getNamedItem(SIDE_B) == null ? null : getDoubleSide(map.getNamedItem(SIDE_B).getNodeValue()),
-                    map.getNamedItem(SIDE_C) == null ? null : getDoubleSide(map.getNamedItem(SIDE_C).getNodeValue()),
-            };
-        }
+        TestXmlFileReader testXmlFileReader = new TestXmlFileReader();
+        Object[][] result = testXmlFileReader.readSidesXmlFile(NEGATIVE);
         return result;
     }
 
@@ -44,13 +20,4 @@ public class ValidatorTest {
         validator.checkExistenceTriangle(triangle);
     }
 
-    public double getDoubleSide(String side) {
-        if (side.equals("Double.MAX_VALUE")) {
-            return Double.MAX_VALUE;
-        }
-        if (side.equals("Double.MIN_VALUE")) {
-            return Double.MIN_VALUE;
-        }
-        return Double.parseDouble(side);
-    }
 }
